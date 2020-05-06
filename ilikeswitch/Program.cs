4eea87aec8b8ff111f8f508cc138ec0921284a97
inuse918 <s2018s34@e-mirim.hs.kr>
2020-05-06 14:07:09 +0900

    실습일지 7 - if 조건문을 switch 조건문으로 바꾸기

--- ilikeswitch/Program.cs
+++ ilikeswitch/Program.cs
@@ -8,8 +8,35 @@ namespace ilikeswitch
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            Console.Write("학년을 입력하세요: ");
+            int level = int.Parse(Console.ReadLine());
+
+            string print(int printLevel)
+            {
+                return ("수강해야 하는 전공 학점: " + printLevel + "학점");
+            }
+            
+            switch (level)
+            {
+                case 1:
+                    Console.WriteLine(print(12));
+                    break;
+                case 2:
+                    Console.WriteLine(print(18));
+                    break;
+                case 3:
+                    Console.WriteLine(print(10));
+                    break;
+                case 4:
+                    Console.WriteLine(print(18));
+                    break;
+                default:
+                    Console.WriteLine("==========");
+                    break;
+            }
         }
     }
 }
